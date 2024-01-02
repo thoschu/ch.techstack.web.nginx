f73aa8a3a396b1d6fd5a6fde6ac44755faa1ee81
Tom S. <thoschulte@googlemail.com>
2024-01-02 16:02:27 +0100

    update script

--- journalctl-sshguard.sh
+++ journalctl-sshguard.sh
@@ -21,4 +21,4 @@ fi
 
 journalctl -u sshguard --since "$duration" --no-pager
 
-echo "$message $greeting $name - $(hostname -I | awk '{print $1}')"
+echo "$message $greeting $name - $(who | awk 'NR==1 {print $5}') >>> $(hostname -I | awk '{print $1}')"
